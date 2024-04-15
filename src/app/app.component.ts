fe8dc7f2f7cd95a94dbbb562ac9e0a1b4355093b
Elrisson Moura <113268700+Elrissonjr@users.noreply.github.com>
2024-04-15 11:13:21 -0300

    Aula pff Marcos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { HeaderComponent } from './components/header/header.component';
+import { MatToolbar } from '@angular/material/toolbar';
+import { FooterComponent } from './components/footer/footer.component';
+import { ReadAllComponent } from './components/read-all/read-all.component';
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [
+        RouterOutlet,
+        HeaderComponent,
+        FooterComponent,
+        MatToolbar,
+        ReadAllComponent,
+    ]
 })
 export class AppComponent {
   title = 'Faculdade';
